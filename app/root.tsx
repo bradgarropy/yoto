844bc915fcd861cb86be1a2d5ddb73a4e2ec6f52
Brad Garropy <bradgarropy@gmail.com>
2026-04-05 20:52:20 -0500

    fix vertical centering on login page.

--- app/root.tsx
+++ app/root.tsx
@@ -110,7 +110,7 @@ export default function App() {
         <div className="flex min-h-screen flex-col">
             <Header />
 
-            <div className="flex-1">
+            <div className="flex flex-1 flex-col">
                 <Outlet />
             </div>
 
